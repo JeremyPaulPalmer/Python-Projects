59af2e4789e4cd87d710d02f937f912b4fe89570
Paseo92 <duxfan18@gmail.com>
2020-05-15 10:17:38 -0700

    Update checkfiles_func.py and checkfiles_gui.py
    Updated files while working on drill to move files from source to destination.

--- drill_checkfiles/checkfiles_func.py
+++ drill_checkfiles/checkfiles_func.py
@@ -1,7 +1,9 @@
 import os
+import fnmatch
 from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
+import sqlite3
 
 
 
@@ -10,11 +12,51 @@ import tkinter as tk
 import checkfiles_main
 import checkfiles_gui
 
+def close_window(self):
+    self.master.destroy()
+    os._exit(0)
 
 def browseFiles(self):
+    global filename
     filename = filedialog.askdirectory()
     self.txt_browse1.insert(0,filename)
+
+def browseFiles2(self):
+    global filename2
+    filename2 = filedialog.askdirectory()
+    self.txt_browse2.insert(0,filename2)
+
+
+def checkFiles(self):
+    folder_list = os.listdir(filename)
+    desired = '*.txt'
+    matchingText = fnmatch.filter(folder_list, desired)
+    for i in matchingText:
+        dateText = [matchingText, os.path.getmtime(matchingText)]
+    if matchingText:
+        conn = sqlite3.connect('movedFiles.db')
+        with conn:
+            cur = conn.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                col_files TEXT, \
+                col_mtime FLOAT \
+                )")
+            conn.commit()
     
+            for i in abPath:
+                cur.execute("INSERT INTO tbl_files(col_files, col_mtime) VALUES (?), (?)")
+                conn.commit()
+            cur.execute("SELECT col_files, col_mtime FROM tbl_files")
+            varFiles = cur.fetchall()
+            print(varFiles)
+        conn.close()
+    
+        
+
+
+        
+        
 
 
 
--- drill_checkfiles/checkfiles_gui.py
+++ drill_checkfiles/checkfiles_gui.py
@@ -20,11 +20,11 @@ def load_gui(self):
     # Define and place buttons
     self.btn_browse1 = tk.Button(self.master,width=24,height=2,text='Browse...',command=lambda:checkfiles_func.browseFiles(self))
     self.btn_browse1.grid(row=0,column=0,padx=(25,0),pady=(60,0))
-    self.btn_browse2 = tk.Button(self.master,width=24,height=2,text='Browse...',command=lambda:checkfiles_func.browseFiles(self))
+    self.btn_browse2 = tk.Button(self.master,width=24,height=2,text='Browse...',command=lambda:checkfiles_func.browseFiles2(self))
     self.btn_browse2.grid(row=1,column=0,padx=(25,0),pady=(20,0))
-    self.btn_check = tk.Button(self.master,width=24,height=4,text='Check for files...')
+    self.btn_check = tk.Button(self.master,width=24,height=4,text='Check for files...',command=lambda:checkfiles_func.checkFiles(self))
     self.btn_check.grid(row=2,column=0,padx=(25,0),pady=(20,0))
-    self.btn_close = tk.Button(self.master,width=24,height=4,text='Close Program')
+    self.btn_close = tk.Button(self.master,width=24,height=4,text='Close Program', command=lambda:checkfiles_func.close_window(self))
     self.btn_close.grid(row=2,column=5,padx=(0,0),pady=(0,0),sticky=S+E)
 
     
