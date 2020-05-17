c677561f23f4264c07d4d91783089e94961d1bcc
Paseo92 <duxfan18@gmail.com>
2020-05-17 16:38:07 -0700

    Update checkfiles_func.py
    Completed drill assignment

--- drill_checkfiles/checkfiles_func.py
+++ drill_checkfiles/checkfiles_func.py
@@ -34,24 +34,25 @@ def checkFiles(self):
     folder_list = os.listdir(filename)
     desired = '*.txt'
     matchingText = fnmatch.filter(folder_list, desired)
-    modStamp = datetime.datetime.fromtimestamp(os.path.getmtime(oldPath))
-    print(modStamp)
-    for (i,j) in zip(matchingText, modStamp):
-            oldPath = os.path.join(filename, i)
-            newPath = os.path.join(filename2, i)
-            conn = sqlite3.connect('movedFiles.db')
-            with conn:
-                cur = conn.cursor()
-                cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-                    col_files TEXT, \
-                    col_mtime TEXT \
-                    )")
-                cur.execute("INSERT INTO tbl_files(col_files, col_mtime) VALUES (?), (?)")
-                conn.commit()
-            conn.close()
-            shutil.move(oldPath, filename2)
-            print(newPath + ' ' + str(datetime.datetime.fromtimestamp(os.path.getmtime(newPath))))
+    for i in matchingText:
+        oldPath = os.path.join(filename, i)
+        newPath = os.path.join(filename2, i)
+        modStamp = str(datetime.datetime.fromtimestamp(os.path.getmtime(oldPath)))
+        conn = sqlite3.connect('movedFiles.db')
+        with conn:
+            cur = conn.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                col_files TEXT, \
+                col_mtime TEXT \
+                )")
+            conn.commit()
+            cur.execute("INSERT INTO tbl_files (col_files, col_mtime) VALUES (?,?)", \
+                        (i, modStamp))
+            conn.commit()
+        conn.close()
+        shutil.move(oldPath, filename2)
+        print(newPath + ' ' + str(datetime.datetime.fromtimestamp(os.path.getmtime(newPath))))
     
     
     
