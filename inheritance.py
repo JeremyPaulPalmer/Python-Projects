b8ed30f85b246dde7d04ad01201a1ff521fbfe9e
Paseo92 <duxfan18@gmail.com>
2020-05-30 23:03:54 -0700

    Create inheritance.py
    Created for inheritance assignment

---
+++ inheritance.py
@@ -0,0 +1,21 @@
+# Python Ver:   3.8.2
+#
+# Purpose:      Inheritance Assignment
+
+
+
+
+class User:
+    name = 'No Name'
+    email = ' '
+    phone_number = ' '
+
+class Student(User):
+    GPA = 0.00
+    Grade_Level = 0
+    Student_ID = 0
+
+class Teacher(User):
+    Room = 0
+    Grade = 0
+    Teacher_ID = 0
