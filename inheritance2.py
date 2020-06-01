e434149c5b0487c3fb032a985a02501307061231
Paseo92 <duxfan18@gmail.com>
2020-06-01 09:01:45 -0700

    Update inheritance2.py
    Updated with working code

--- inheritance2.py
+++ inheritance2.py
@@ -6,15 +6,16 @@
 
 
 class User:
-    name = 'No Name'
-    email = ' '
-    phone_number = ' '
-    password = ' '
+    name = 'Jeremy Palmer'
+    email = 'duxfan18@gmail.com'
+    phone_number = '5037018554'
+    password = 'TechAcademy'
 
     def LoginInfo(self):
+        input_name = input("Please enter your name: ")
         input_email = input("Please enter your email: ")
         input_pw = input("Please enter your password: ")
-        if (input_email == self.email and input_password == self.password):
+        if (input_email == self.email and input_pw == self.password):
             print("Welcome back, {}!".format(input_name))
         else:
             print("Please re-enter your login credentials.")
@@ -22,13 +23,15 @@ class User:
 class Student(User):
     GPA = 0.00
     Grade_Level = 0
-    Student_ID = 0
+    Student_ID = "18"
 
     def LoginInfo(self):
-        input_ID = input("Please enter you Student ID: ")
+        input_name = input("Please enter your name: ")
+        input_email = input("Please enter your email: ")
         input_pw = input("Please enter your password: ")
-        if (input_ID == self.Student_ID and input_pw == self.password):
-            print("Welcome back, {}!".format(self.name))
+        input_ID = input("Please enter your Student ID: ")
+        if (input_email == self.email and input_pw == self.password and input_ID == self.Student_ID):
+            print("Welcome back, {}!".format(input_name))
         else:
             print("Please re-enter your login credentials.")
 
